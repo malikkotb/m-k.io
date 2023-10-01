53b1ac1dcf3bcba3877e665f042a913eb66b2c47
Malik Kotb <malikkotb@icloud.com>
2023-10-01 15:54:54 +0200

    tutorial page structure

--- app/tutorials.js
+++ app/tutorials.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function tutorials() {
+  const tutorials = [
+    { title: "Tutorial 1", content: "content 1" },
+    { title: "Tutorial 2", content: "Content 2" },
+    { title: "Tutorial 3", content: "Content 3" },
+  ];
   return (
     <div>
-        
+      <h1 className="text-3xl text-center">Tutorials</h1>
+      <ul>
+        {blogs.map((blog, index) => (
+          <li key={index}>
+            <h2>{blog.title}</h2>
+            <p>{blog.content}</p>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
