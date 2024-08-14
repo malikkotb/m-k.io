b13d8bde4ab45a3a219ab06ea9605732090ef117
malikkotb <malikkotb@icloud.com>
2024-08-14 23:10:39 +0200

    1

--- app/page.js
+++ app/page.js
@@ -10,37 +10,37 @@ export default function Home() {
       title1: "Note",
       title2: "Taker",
       src: "./dark-mode-with-coding.png",
-      link: "/projects/notetaker"
+      link: "/projects/notetaker",
     },
     {
       title1: "Sticky",
       title2: "Notes",
       src: "./sticky_notes.png",
-      link: "/projects/sticky"
+      link: "/projects/sticky",
     },
     {
       title1: "Pastry",
       title2: "Shop",
       src: "./pastry_shop.png",
-      link: "/projects/pastryshop"
+      link: "/projects/pastryshop",
     },
     {
       title1: "Food",
       title2: "Pal",
       src: "./foodpal.png",
-      link: "/projects/foodpal"
+      link: "/projects/foodpal",
     },
     {
       title1: "Vue",
       title2: "Chat",
       src: "./vue_chat.png",
-      link: "/projects/chatapp"
+      link: "/projects/chatapp",
     },
     {
       title1: "PyTorch",
       title2: "CNN",
       src: "./image_cnn.png",
-      link: "https://github.com/malikkotb/Seminar_SS23"
+      link: "https://github.com/malikkotb/Seminar_SS23",
     },
   ];
 
@@ -53,19 +53,54 @@ export default function Home() {
     <div className="flex flex-col gap-16 md:gap-24">
       <div className="flex flex-col gap-8">
         <div>
-          <motion.h1 variants={animationVariant} initial="hidden" animate="visible" transition={{ delay: 0.05, duration: 0.35 }} 
-          className="text-3xl font-bold">Malik Kotb</motion.h1>
-          <motion.p variants={animationVariant} initial="hidden" animate="visible" transition={{ delay: 0.1, duration: 0.35 }} className="text-gray-500">I do cool things with code.</motion.p>
+          <motion.h1
+            variants={animationVariant}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 0.05, duration: 0.35 }}
+            className="text-3xl font-bold"
+          >
+            Malik Kotb
+          </motion.h1>
+          <motion.p
+            variants={animationVariant}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 0.1, duration: 0.35 }}
+            className="text-gray-500"
+          >
+            I do cool things with code.
+          </motion.p>
         </div>
         <div className="flex flex-col md:flex-row gap-6 md:items-center">
-          <motion.img variants={animationVariant} initial="hidden" animate="visible" transition={{ delay: 0.15, duration: 0.35 }} src="profile.png" alt="profile" className="w-40 rounded-full"></motion.img>
+          <motion.img
+            variants={animationVariant}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 0.15, duration: 0.35 }}
+            src="profile.png"
+            alt="profile"
+            className="w-40 rounded-full"
+          ></motion.img>
         </div>
-        <motion.p variants={animationVariant} initial="hidden" animate="visible" transition={{ delay: 0.2, duration: 0.35 }} className="  max-w-lg">
-          Hi, I&apos;m Malik Kotb, a software engineer who loves building cool things
-          with code. I am currently writing my bachelor&apos;s thesis. I studied Computer Science:
-          Games Engineering at TUM.
+        <motion.p
+          variants={animationVariant}
+          initial="hidden"
+          animate="visible"
+          transition={{ delay: 0.2, duration: 0.35 }}
+          className="  max-w-lg"
+        >
+          Hi, I&apos;m Malik Kotb, a software engineer who loves building cool things with
+          code. I am currently focusing on creative frontend development. I studied
+          Computer Science: Games Engineering at TUM.
         </motion.p>
-        <motion.ul variants={animationVariant} initial="hidden" animate="visible" transition={{ delay: 0.25, duration: 0.35 }} className="flex flex-col md:flex-row gap-2 md:gap-6">
+        <motion.ul
+          variants={animationVariant}
+          initial="hidden"
+          animate="visible"
+          transition={{ delay: 0.25, duration: 0.35 }}
+          className="flex flex-col md:flex-row gap-2 md:gap-6"
+        >
           <li>
             <a
               className="transition-transform transform hover:translate-y-[-1px] hover:translate-x-[1px]
@@ -89,14 +124,26 @@ export default function Home() {
         </motion.ul>
       </div>
       <div className="flex flex-col">
-        <motion.p variants={animationVariant} initial="hidden" animate="visible" transition={{ delay: 0.3, duration: 0.35 }} className="pb-4">Projects</motion.p>
-        <motion.div variants={animationVariant} initial="hidden" animate="visible" transition={{ delay: 0.35, duration: 0.35 }}>
-        {projects.map((project, index) => {
-          return <Project key={index} project={project} link={project.link} />;
-        })}
+        <motion.p
+          variants={animationVariant}
+          initial="hidden"
+          animate="visible"
+          transition={{ delay: 0.3, duration: 0.35 }}
+          className="pb-4"
+        >
+          Projects
+        </motion.p>
+        <motion.div
+          variants={animationVariant}
+          initial="hidden"
+          animate="visible"
+          transition={{ delay: 0.35, duration: 0.35 }}
+        >
+          {projects.map((project, index) => {
+            return <Project key={index} project={project} link={project.link} />;
+          })}
         </motion.div>
       </div>
-      
     </div>
   );
 }
